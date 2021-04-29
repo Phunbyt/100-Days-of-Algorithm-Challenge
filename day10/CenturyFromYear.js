09797c9da48ac925d9afb92eba7f1599f6cbf51a
Phunbyt <47262310+Phunbyt@users.noreply.github.com>
2021-04-29 23:35:52 +0100

    day10>>>

--- day10/CenturyFromYear.js
+++ day10/CenturyFromYear.js
@@ -1,13 +1,14 @@
 function century(year) {
-    let count = 0
-    let x = year
-        // Finish this :)
+    let count = 0;
+    // Finish this :)
     function club(x) {
         if (x < 101) return count++;
         count++;
         x = x - 100;
+        console.log(x)
         club(x);
     }
     club(year);
     return count;
 }
+console.log(century(1900))
--- day10/Mumbling.js
+++ day10/Mumbling.js
@@ -5,9 +5,11 @@ function accum(s) {
     for (let i = 1; i <= s.length; i++) {
         let stack = ''
         stack += s[i - 1].repeat(i)
+        stack.substring(0, 1).toUpperCase()
         let popper = [...stack.toLowerCase()]
         let popp = popper.shift().toUpperCase() + popper.join("")
         newArr.push(popp)
     }
     return newArr.join("-")
 }
+accum('abc')
